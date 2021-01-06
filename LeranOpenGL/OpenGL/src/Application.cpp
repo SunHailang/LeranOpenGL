f0d275f32f0498f78073cdd2b56c6d39a03e97e4
HaiLangSun <hai-lang.sun@ubisoft.com>
2021-01-06 17:47:31 +0800

    init opengl labriary

---
+++ LeranOpenGL/OpenGL/src/Application.cpp
@@ -0,0 +1,86 @@
+﻿#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "glew init failed." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	std::cout << "[OpenGL Version]: " << glfwGetVersionString() << std::endl;
+
+	// 绘制三角形
+	float positions[] = {
+		0.5f, 0.5f,
+		0.5f, -0.5f,
+		-0.5f, 0.5f,
+		-0.5f, -0.5f
+	};
+
+	unsigned int indexs[] = {
+		0, 1, 2,
+		1, 2, 3
+	};
+
+	unsigned int vao;
+	unsigned int vbo;
+	unsigned int veo;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &veo);
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexs), indexs, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 0.2f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &veo);
+
+	glfwTerminate();
+	return 0;
+}
