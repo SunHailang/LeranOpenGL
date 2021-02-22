a4053837d629b6039e6669e8b410a86bc5e7d344
HaiLangSun <hai-lang.sun@ubisoft.com>
2021-02-22 19:11:12 +0800

    update game engine

--- LeranOpenGL/OpenGLGameEngine/src/Hazel.h
+++ LeranOpenGL/OpenGLGameEngine/src/Hazel.h
@@ -3,6 +3,7 @@
 // for use by Hazel appliction
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // -- Entry Point ---------
 #include "Hazel/EntryPoint.h"
--- LeranOpenGL/OpenGLGameEngine/src/Hazel/EntryPoint.h
+++ LeranOpenGL/OpenGLGameEngine/src/Hazel/EntryPoint.h
@@ -6,6 +6,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialtion Log!");
+
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
+
 	Hazel::Application* app = Hazel::CreateApplication();
 
 	app->Run();
